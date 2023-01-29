8cf21406bd38b89b84d0ce4ed4f06bb202c89211
mujahidul <mujahidul@softwrd.ai>
2023-01-29 22:01:04 +0600

    basic params operation

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func nameHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.JSON(200, gin.H{
+		"message": "hello" + name,
+	})
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "hello world"})
-	})
+	router.GET("/:name ", nameHandler)
 	router.Run()
 }
