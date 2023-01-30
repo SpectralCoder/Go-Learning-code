f08186f6ee6765ba9903c4751f3729681bfc7239
mujahidul <mujahidul@softwrd.ai>
2023-01-30 22:09:26 +0600

    swagger intro

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
+// Recipes API
+
+//
+
+// This is a sample recipes API. You can find out more about the API at https://github.com/PacktPublishing/Building-Distributed-Applications-in-Gin.
+
+//
+
+//  Schemes: http
+
+//  Host: localhost:8080
+
+//  BasePath: /
+
+//  Version: 1.0.0
+
+//  Contact: Mohamed Labouardy
+
+// <mohamed@labouardy.com> https://labouardy.com
+
+//
+
+//  Consumes:
+
+//  - application/json
+
+//
+
+//  Produces:
+
+//  - application/json
+
+// swagger:meta
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -12,6 +48,8 @@ var recipes []Recipe
 
 func init() {
 	recipes = make([]Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
 }
 
 type Recipe struct {
@@ -40,9 +78,108 @@ func ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
 }
 
+// swagger:operation PUT /recipes/{id} recipes updateRecipe
+
+// Update an existing recipe
+
+// ---
+
+// parameters:
+
+// - name: id
+
+//   in: path
+
+//   description: ID of the recipe
+
+//   required: true
+
+//   type: string
+
+// produces:
+
+// - application/json
+
+// responses:
+
+//     '200':
+
+//         description: Successful operation
+
+//     '400':
+
+//         description: Invalid input
+
+//     '404':
+
+//         description: Invalid recipe ID
+
+func updateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+			break
+		}
+	}
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "recipe not found"})
+		return
+	}
+	recipes[index] = recipe
+	c.JSON(http.StatusOK, recipe)
+}
+
+func deleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+			break
+		}
+	}
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "recipe not found"})
+		return
+	}
+	recipes = append(recipes[:index], recipes[index+1:]...)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe has been deleted"})
+}
+
+func searchRecipesHandler(c *gin.Context) {
+	tag := c.Query("tag")
+	listOfRecipes := make([]Recipe, 0)
+	for i := 0; i < len(recipes); i++ {
+		found := false
+		for _, t := range recipes[i].Tags {
+			if strings.EqualFold(t, tag) {
+				found = true
+			}
+		}
+		if found {
+			listOfRecipes = append(listOfRecipes, recipes[i])
+		}
+	}
+	c.JSON(http.StatusOK, listOfRecipes)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/recipes", ListRecipesHandler)
 	router.POST("/recipes", NewRecipeHandler)
+	router.PUT("/recipe/:id", updateRecipeHandler)
+	router.DELETE("/recipe/:id", deleteRecipeHandler)
+	router.GET("/recipes/search", searchRecipesHandler)
 	router.Run()
 }
